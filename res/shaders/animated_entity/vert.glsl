08ffa0caf80c506b6364f42515731bf67dbcdff2
thomas rigby <tomr0607@hotmail.com>
2023-05-16 17:02:15 +0800

    fixed D - drag material slider first to see

--- res/shaders/animated_entity/vert.glsl
+++ res/shaders/animated_entity/vert.glsl
@@ -22,7 +22,7 @@ uniform vec3 diffuse_tint;
 uniform vec3 specular_tint;
 uniform vec3 ambient_tint;
 uniform float shininess;
-uniform float textureScale; 
+uniform float textureScale;
 
 // Light Data
 #if NUM_PL > 0
@@ -56,7 +56,7 @@ void main() {
 
     vec3 ws_position = (animation_matrix * vec4(vertex_position, 1.0f)).xyz;
     vec3 ws_normal = normalize(normal_matrix * normal);
-    vertex_out.texture_coordinate = texture_coordinate;
+    vertex_out.texture_coordinate = texture_coordinate*textureScale;
 
     gl_Position = projection_view_matrix * vec4(ws_position, 1.0f);
 
